cfce642af1801f2ec7b166068d19b92bfb1670ae
Engy Ahmed Abdelaziz <engya306@gmail.com>
2022-10-05 01:44:12 +0200

    BMI

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bmi_calculator/bmi_result_screen.dart';
+import 'package:bmi_calculator/home_Screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
